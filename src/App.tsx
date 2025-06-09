a6bb8155a32c0adfa602637ff5b072af066830ff
Inter2025-web <consumptions2025@gmail.com>
2025-06-08 19:26:10 -0500

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '3rem' }}>
+      <h1>Room Booking</h1>
+      <p>¡Tu aplicación está funcionando correctamente en Vercel!</p>
+    </div>
+  );
+}
+
+export default App;
